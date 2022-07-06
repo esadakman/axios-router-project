dbf60b9c0f4c25ef08210a725610f88d8470a720
esadakman <esadd26@gmail.com>
2022-07-06 19:40:06 +0300

    paginate finished

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,39 +1,31 @@
-import React from "react";
-import { useState } from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import { NavLink } from "react-router-dom";
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {NavLink} from "react-router-dom"
+
 const NavBar = () => {
-  const [isOpen, setIsOpen] = useState(
-    sessionStorage.getItem("email") || false
-  );
-  const handleClick = () => {
-    setIsOpen(false);
-    sessionStorage.clear();
-  };
+  const [isOpen,setIsOpen]=React.useState(sessionStorage.getItem("email") || false)
+
+  const handleClick=()=>{
+    setIsOpen(false)
+    sessionStorage.clear()
+  }
   return (
     <div>
       <Navbar bg="success" variant="dark">
-        {isOpen ? (
-          <Container>
-            <Navbar.Brand to="/home">Clarusway</Navbar.Brand>
-            <Nav className="me-auto">
-              <NavLink to="/home">Home</NavLink>
-              <NavLink to="/about">About</NavLink>
-              <NavLink to="/" onClick={handleClick}>
-                Logout
-              </NavLink>
-            </Nav>
-          </Container>
-        ) : (
-          <Container>
-            <Navbar.Brand to="#home">Clarusway</Navbar.Brand>
-          </Container>
-        )}
+        {isOpen ? (<Container>
+          <Navbar.Brand href="/home">Clarusway</Navbar.Brand>
+          <Nav className="me-auto">
+            <NavLink to="/home">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/" onClick={handleClick}>Logout</NavLink>
+          </Nav>
+        </Container>):(<Container>
+          <Navbar.Brand href="">Clarusway</Navbar.Brand></Container>)}
       </Navbar>
     </div>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,37 @@
-import axios from "axios";
-import React, { useState } from "react";
-import { useEffect } from "react";
-import Followers from "../components/Followers";
-import SearchUser from "../components/SearchUser";
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import SearchUser from "../components/SearchUser"
+import Followers from "../components/Followers"
 
 const Home = () => {
-  const [allFollowers, setAllFollowes] = useState([]);
+  const [allFollowers,setAllFollowers]=useState([])
+  const [loading,setLoading]=useState(true)
+  const [search,setSearch]=useState("")
 
-  const [loading, setLoading] = useState(true);
-  const [search, setSearch] = useState("");
+  const getFollowers = async()=>{
+    const {data} = await axios.get("https://api.github.com/users/esadakman/followers?per_page=100")
+    setAllFollowers(data)
+    console.log(data)
+  }
+  const handleChange=(e)=>{
+    setSearch(e.target.value)
+    console.log(e.target.value)
+  }
+  useEffect(()=>{
+    getFollowers()
+    setTimeout(()=>{
+      setLoading(false)
+    },3000)
+  },[])
 
-  const getFollowers = async () => {
-    const { data } = await axios.get(
-      "https://api.github.com/users/esadakman/followers?per_page=100"
-    );
-    console.log(data);
-    setAllFollowes(data);
-  };
-
-  const handleChange = (e) => {
-    setSearch(e.target.value);
-    console.log(e.target.value);
-  };
-
-  useEffect(() => {
-    getFollowers();
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
-
-  const followersList = allFollowers.filter((follower) =>
-    follower.login.includes(search)
-  );
+  const followersList= allFollowers.filter(follower=>follower.login.includes(search))
 
   return (
     <div>
-      <SearchUser handleChange={handleChange} />
-      <Followers followers={{ followersList, loading }} />
+      <SearchUser handleChange={handleChange}/>
+      <Followers followers={{loading,followersList}} />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
